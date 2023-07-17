ad8e5f22596da6c8c83577e8bf62f09ec7bc9881
James Yuill <jamesyuill@jamess-mbp.lan>
2023-07-17 16:33:25 +0100

    updates cards with comment count and created at

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -4,6 +4,8 @@ export const ArticleCard = ({ article }) => {
       <h3>{article.title}</h3>
       <img src={article.article_img_url} alt={article.title} />
       <p>Topic: {article.topic}</p>
+      <p>Date posted: {article.created_at}</p>
+      <p>Comment count: {article.comment_count}</p>
     </div>
   );
 };
