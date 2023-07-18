bce24ac7a0c9f554a8453571b9c28eb1a986428c
James Yuill <jamesyuill@jamess-mbp.lan>
2023-07-18 13:58:15 +0100

    updates task 8 with newComment state for input fields

--- src/components/NewCommentForm.jsx
+++ src/components/NewCommentForm.jsx
@@ -1,10 +1,31 @@
+import { useState } from 'react';
+
 export const NewCommentForm = () => {
+  const [newComment, setNewComment] = useState({
+    username: '',
+    body: '',
+  });
+
+  const updateFormField = (field, value) => {
+    setNewComment((curr) => ({ ...curr, [field]: value }));
+  };
+
   return (
     <form className="new-comment-form">
       <label htmlFor="username">Username: </label>
-      <input type="text" id="username" />
+      <input
+        type="text"
+        id="username"
+        value={newComment.username}
+        onChange={(e) => updateFormField('username', e.target.value)}
+      />
       <label htmlFor="comment-textarea">Comment: </label>
-      <textarea id="comment-textarea" maxLength={150} />
+      <textarea
+        id="comment-textarea"
+        maxLength={150}
+        value={newComment.body}
+        onChange={(e) => updateFormField('body', e.target.value)}
+      />
       <button>Submit!</button>
     </form>
   );
