079986c72d8c2f976d6b467233055505152870fb
James Yuill <jamesyuill@jamess-mbp.lan>
2023-07-18 11:20:43 +0100

    updates task 7 with some buttons and not much logic

--- src/components/VotesComp.jsx
+++ src/components/VotesComp.jsx
@@ -1,7 +1,32 @@
+import { useState } from 'react';
+
 export const VotesComp = ({ article: { votes } }) => {
+  const [updateVote, setUpdateVote] = useState(votes);
+
+  function handleClick(num) {
+    // e.preventDefault();
+    setUpdateVote((curr) => {
+      return (curr += num);
+    });
+  }
+
   return (
     <div className="votes-comp">
-      <p>Votes: {votes}</p>
+      <p>Votes: {updateVote}</p>
+      <button
+        onClick={() => {
+          handleClick(1);
+        }}
+      >
+        Up Vote
+      </button>
+      <button
+        onClick={() => {
+          handleClick(-1);
+        }}
+      >
+        Down Vote
+      </button>
     </div>
   );
 };
