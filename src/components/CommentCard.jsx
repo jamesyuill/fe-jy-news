7381c28c53f6ef50bf29212088af769f29026299
James Yuill <jamesyuill@jamess-mbp.lan>
2023-07-20 12:32:41 +0100

    updates task 11 with functioning delete button on specific users comments

--- src/components/CommentCard.jsx
+++ src/components/CommentCard.jsx
@@ -1,12 +1,14 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import formatDateTime from '../utils/formatDateTime';
 import { UserContext } from '../contexts/User';
+import { deleteCommentById } from '../utils/api';
 
 export const CommentCard = ({
   comment: { author, body, created_at, votes, comment_id },
   setComments,
 }) => {
   const { user, setUser } = useContext(UserContext);
+  const [isError, setIsError] = useState(false);
 
   const dateAndTime = formatDateTime(created_at);
 
@@ -15,7 +17,20 @@ export const CommentCard = ({
     setComments((curr) => {
       return curr.filter((comment) => comment.comment_id !== comment_id);
     });
+    deleteCommentById(comment_id).catch((err) => {
+      setIsError(true);
+    });
   }
+
+  // useEffect(() => {
+  //   deleteCommentById(comment_id)
+  //     .then(() => {
+  //       console.log('delete worked');
+  //     })
+  //     .catch((err) => {
+  //       setIsError(true);
+  //     });
+  // }, [deleteClicked]);
 
   return (
     <article className="comment-card">
